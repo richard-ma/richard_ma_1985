f0bafad465973b87b38f30ce0aa7a805c9c0edc3
Richard Ma <richard.ma.19850509@gmail.com>
2015-02-25 03:29:16 +0800

    use fallback to sign

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -1,17 +1,19 @@
 class WechatsController < ApplicationController
-  def index
-    if params['echostr']
-      @echostr = params['echostr']
-    else
-      @echostr = 'hello, This is richard-ma-1985'
-    end
+  #def index
+    #if params['echostr']
+      #@echostr = params['echostr']
+    #else
+      #@echostr = 'hello, This is richard-ma-1985'
+    #end
 
-    render layout: "wechats"
-  end
+    #render layout: "wechats"
+  #end
 
   wechat_responder
 
   on :text do |request, content|
     request.reply.text "echo: #{content}"
   end
+
+  on :fallback, respond: params['echostr']
 end
