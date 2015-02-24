15256bc85f5f163adcb1aa954315b2eda4079766
Richard Ma <richard.ma.19850509@gmail.com>
2015-02-25 02:52:48 +0800

    add index action

--- app/controllers/wechat_controller.rb
+++ app/controllers/wechat_controller.rb
@@ -1,9 +1,15 @@
 class WechatController < ApplicationController
+  def index
+    if params['echostr']
+      p params['echostr']
+    else
+      p 'hello, This is richard-ma-1985'
+    end
+  end
+
   wechat_responder
 
   on :text do |request, content|
     request.reply.text "echo: #{content}"
   end
-
-  on :fallback, respond: params['echostr']
 end
